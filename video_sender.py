01d70e4be922d2f6565c3d5fd5006f02e60ca710
TulqinWeb <tulqinurinov005@gmail.com>
2024-12-06 12:28:55 +0500

    some changes added to send message to user

--- video_sender.py
+++ video_sender.py
@@ -1,5 +1,4 @@
 import requests
-
 from Downloader import download_instagram_media
 
 
@@ -10,20 +9,26 @@ async def send_video(update, context):
         await update.message.reply_text("Iltimos, Instagram URL yuboring.")
         return
 
-    await update.message.reply_text("Instagram media yuklanmoqda...")
+    if 'instagram.com' in url:
+        await update.message.reply_text("Instagram media yuklanmoqda...")
 
-    try:
-        # Instagramdan barcha media fayllarni olish
-        media_files = await download_instagram_media(url)
+        try:
+            # Instagramdan barcha media fayllarni olish
+            media_files = await download_instagram_media(url)
 
-        # Har bir media faylni foydalanuvchiga yuborish
-        for media_url, media_type in media_files:
-            response = requests.get(media_url, stream=True)
-            if media_type == 'video/mp4':
-                await update.message.reply_video(response.content)
-            else:
-                await update.message.reply_photo(response.content)
+            # Har bir media faylni foydalanuvchiga yuborish
+            for media_url, media_type in media_files:
+                response = requests.get(media_url, stream=True)
+                if media_type == 'video/mp4':
+                    await update.message.reply_video(response.content, caption='@InstaMelodyBot')
+                else:
+                    await update.message.reply_photo(response.content)
 
 
-    except Exception as e:
-        await update.message.reply_text(f"Xatolik yuz berdi: {str(e)}")
+        except Exception as e:
+            print(f"Xatolik yuz berdi: {str(e)}")
+
+    else:
+        await update.message.reply_text(text='Bu url instagramga tegishli emas!')
+
+
